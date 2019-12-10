bf3e723a10e153b034eb41c69e6ef7266cd18393
Yusuke Yonekura <bimadisiwin@me.com>
2019-12-10 16:27:55 +0700

    ヘッダーメニューの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def header_link_item(name, path)
+    class_name = 'nav-item'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class: class_name do
+      link_to name, path, class: 'nav-link'
+    end
+  end
 end
